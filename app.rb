0997e5ab0c73d93b0c9574a2b474c37eb48a9aef
Josue Valverde <student@epicodus.com>
2015-08-21 14:49:44 -0700

    add routes to client side editing and displaying.

--- app.rb
+++ app.rb
@@ -4,13 +4,15 @@ require('./lib/client')
 require('./lib/stylist')
 require('pg')
 also_reload('/lib/**/*.rb')
+require('pry')
 
-# DB = PG.connect({:dbname => 'hair_salon'})
+DB = PG.connect({:dbname => 'hair_salon'})
 
 get('/') do
     erb(:index)
 end
 
+###### Stylists Side #######
 get('/stylists') do
     @stylists = Stylist.all()
     erb(:stylists)
@@ -25,7 +27,78 @@ post('/stylists') do
     erb(:stylists)
 end
 
+patch('/stylists') do
+    name = params['name']
+    specialty = params['specialty']
+    rating = params['rating'].to_i
+    id = params['id'].to_i
+    stylist = Stylist.find(id)
+    stylist.update({:name => name, :specialty => specialty, :rating => rating})
+    @stylists = Stylist.all()
+    erb(:stylists)
+end
+
+get('/stylists/:id') do
+    id = params['id'].to_i
+    @stylist = Stylist.find(id)
+    erb(:stylist_edit)
+end
+
+get('/stylists/:id/delete') do
+    id = params['id'].to_i
+    stylist = Stylist.find(id)
+    stylist.delete
+    @stylists = Stylist.all()
+    erb(:stylists)
+end
+
+get('/stylists/:id/clients') do
+    id = params['id'].to_i
+    @stylists = Stylist.find(id)
+    erb(:stylist_clients)
+end
+
+post('/stylists/:id/clients') do
+    id = params['id'].to_i
+    @stylists = Stylist.find(id)
+    name = params['name']
+    hairtype = params['hairtype']
+    rating = params['rating'].to_i
+    client = Client.new({:id => nil, :stylist_id => @stylists.id(), :name => name, :hairtype => hairtype, :rating => rating})
+    client.save()
+    erb(:stylist_clients)
+end
+#########Client Side ############
 get('/clients') do
-    @client = Client.all()
+    @clients = Client.all()
+    erb(:clients)
+end
+
+patch('/clients') do
+    client = Client.find(params['id'].to_i)
+    name = params['name']
+    hairtype = params['hairtype']
+    stylist_id = params['stylist_id'].to_i
+    rating = params['rating'].to_i
+    client.update({:name => name, :hairtype => hairtype, :stylist_id => stylist_id, :rating => rating})
+    @clients = Client.all()
+    erb(:clients)
+end
+
+get('/clients/:id') do
+    @client = Client.find(params['id'].to_i)
+    @stylists = Stylist.all
+    erb(:client_edit)
+end
+
+patch('/clients/:id') do
+    @stylists = Stylist.all
+    client = Client.find(params['id'].to_i)
+    name = params['name']
+    hairtype = params['hairtype']
+    rating = params['rating'].to_i
+    stylist_id = params['stylist_id'].to_i
+    client.update({:name => name, :hairtype => hairtype, :rating => rating, :stylist_id => stylist_id})
+    @clients = Client.all
     erb(:clients)
 end
