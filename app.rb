66609bdadcd26dab9d2e42f4b30fb488ba093c60
Josue Valverde <student@epicodus.com>
2015-08-21 12:25:18 -0700

    add post fetch for /stylists page.

--- app.rb
+++ app.rb
@@ -5,16 +5,27 @@ require('./lib/stylist')
 require('pg')
 also_reload('/lib/**/*.rb')
 
-# DB = PG.connect({:dbname => 'hair_salon'})
+DB = PG.connect({:dbname => 'hair_salon'})
 
 get('/') do
     erb(:index)
 end
 
 get('/stylists') do
+    @stylists = Stylist.all()
+    erb(:stylists)
+end
+
+post('/stylists') do
+    name = params['name']
+    specialty = params['specialty']
+    stylist = Stylist.new({:id => nil, :name => name, :specialty => specialty, :rating => 0})
+    stylist.save()
+    @stylists = Stylist.all()
     erb(:stylists)
 end
 
 get('/clients') do
+    @client = Client.all()
     erb(:clients)
 end
