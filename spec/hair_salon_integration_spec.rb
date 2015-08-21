75f04b87a5fcec75055df6778a51a4a33cc1f9b6
Josue Valverde <student@epicodus.com>
2015-08-21 15:47:23 -0700

    add and pass spec that checks that a client is added and displayed.

--- spec/hair_salon_integration_spec.rb
+++ spec/hair_salon_integration_spec.rb
@@ -18,7 +18,7 @@ describe('the hair salon webapp', {:type => :feature}) do
     it('properly displays client manager page') do
         visit('/')
         click_link('Client Manager')
-        expect(page).to have_content('Client Manager')
+        expect(page).to have_content('Clients')
     end
 
     it('properly adds and displays a new stylist') do
@@ -26,6 +26,14 @@ describe('the hair salon webapp', {:type => :feature}) do
         fill_in('name', :with => 'Mike')
         fill_in('specialty', :with => 'Dyeing')
         click_button('Add Stylist')
+        expect(page).to have_content('Mike')
+    end
+
+    it('properly adds and displays a new client') do
+        visit('/clients')
+        fill_in('name', :with => 'Mike')
+        fill_in('hairtype', :with => 'Dyeing')
+        click_button('Add Client')
         expect(page).to have_content('Mike')
     end
 end
