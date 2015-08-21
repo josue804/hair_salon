fcb56982c863d34675f145497ceda045c92737fb
Josue Valverde <student@epicodus.com>
2015-08-21 10:10:31 -0700

    add and pass spec that saves a stylist onto the database.

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -2,22 +2,31 @@ class Stylist
     attr_reader(:id, :name, :specialty, :rating)
 
     define_method(:initialize) do |attributes|
-        @id = attributes['id']
-        @name = attributes['name']
-        @specialty = attributes['specialty']
-        @rating = attributes['rating']
+        @id = attributes[:id]
+        @name = attributes[:name]
+        @specialty = attributes[:specialty]
+        @rating = attributes[:rating]
     end
 
     define_singleton_method(:all) do
         returned_stylists = DB.exec("SELECT * FROM stylists;")
         stylists = []
-        returned_stylists.each() do |stylist|
-            @id = stylist['id'].to_i()
-            @name = stylist['name']
-            @specialty = stylist['specialty']
-            @rating = stylist['rating'].to_i()
-            stylists.push(Stylist.new({:id => @id, @name => name, @specialty => specialty, @rating => rating}))
+        returned_stylists.each do |stylist|
+            id = stylist['id'].to_i
+            name = stylist['name']
+            specialty = stylist['specialty']
+            rating = stylist['rating'].to_i
+            stylists.push(Stylist.new({:id => id, :name => name, :specialty => specialty, :rating => rating}))
         end
         stylists
+    end
+
+    define_method(:==) do |other|
+        name == other.name && specialty == other.specialty && rating == other.rating
+    end
+
+    define_method(:save) do
+        result = DB.exec("INSERT INTO stylists (name, specialty, rating) VALUES ('#{@name}', '#{@specialty}', #{@rating}) RETURNING id;")
+        @id = result.first.fetch('id').to_i
     end
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -3,12 +3,26 @@ require('spec_helper')
 describe(Stylist) do
 
     before() do
-        @stylist = Stylist.new({:id => nil, :stylist_id => 1, :name => "Jeff"})
+        @stylist = Stylist.new({:id => nil, :name => "Jeff", :specialty => "Curly", :rating => 4})
     end
 
-    describe('#all') do
+    describe('.all') do
         it('returns nothing at first') do
             expect(Stylist.all()).to(eq([]))
+        end
+    end
+
+    describe('#==') do
+        it('overrides the equality operator to only compare non-id attributes') do
+            stylist_2 = Stylist.new({:id => nil, :stylist_id => 1, :name => "Jeff", :specialty => "Curly", :rating => 4})
+            expect(@stylist).to(eq(stylist_2))
+        end
+    end
+
+    describe('#save') do
+        it('saves a stylist onto the stylists table') do
+            @stylist.save()
+            expect(Stylist.all()).to(eq([@stylist]))
         end
     end
 end
