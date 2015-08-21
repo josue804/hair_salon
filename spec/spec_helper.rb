130e8fa1f7853543a94356fe1f1d8dad4d3f1ee2
Josue Valverde <student@epicodus.com>
2015-08-21 12:27:29 -0700

    comment out connection to test database to test non-test database manually.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,11 @@ require('client')
 require('stylist')
 require('pg')
 
-DB = PG.connect({:dbname => 'hair_salon_test'})
+# DB = PG.connect({:dbname => 'hair_salon_test'})
 
-RSpec.configure do |config|
-    config.after(:each) do
-        DB.exec('DELETE FROM stylists')
-        DB.exec('DELETE FROM clients')
-    end
-end
+# RSpec.configure do |config|
+#     config.after(:each) do
+#         DB.exec('DELETE FROM stylists')
+#         DB.exec('DELETE FROM clients')
+#     end
+# end
